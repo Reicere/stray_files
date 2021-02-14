2a77856667aa10a69dd78e65cb8900764214af34
Reicere <39677513+Reicere@users.noreply.github.com>
2021-02-13 23:04:54 -0500

    Update and rename com.aurora.store.Android.mk to com.aurora.services.Android.mk

--- v410/microg/com.aurora.services.Android.mk
+++ v410/microg/com.aurora.services.Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := privapp-permissions-com.aurora.store.xml
+LOCAL_MODULE := privapp-permissions-com.aurora.services.xml
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
@@ -10,11 +10,11 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := AuroraApplication
-LOCAL_SRC_FILES := com.aurora.store.apk
+LOCAL_MODULE := AuroraServices
+LOCAL_SRC_FILES := com.aurora.services.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_REQUIRED_MODULES := privapp-permissions-com.aurora.store.xml
+LOCAL_REQUIRED_MODULES := privapp-permissions-com.aurora.services.xml
 include $(BUILD_PREBUILT)
